864612c01205ceb477eb03736ac52169b2bab61c
S.P.E.C <spec@nith.ac.in>
2025-08-30 12:30:40 +0530

    vector store and github techstack

---
+++ technologies.py
@@ -0,0 +1,351 @@
+technologies = {
+    "frontend": [
+        # JavaScript Frameworks & Libraries
+        "React",
+        "Angular",
+        "Vue.js",
+        "Svelte",
+        "Preact",
+        "Ember.js",
+        "Backbone.js",
+        "jQuery",
+        "SolidJS",
+        "Qwik",
+        "Alpine.js",
+
+        # CSS Frameworks & Preprocessors
+        "Bootstrap",
+        "Tailwind CSS",
+        "Materialize",
+        "Foundation",
+        "Bulma",
+        "Sass",
+        "Less",
+        "Stylus",
+        "PostCSS",
+
+        # State Management
+        "Redux",
+        "MobX",
+        "Vuex",
+        "Zustand",
+        "Recoil",
+        "Pinia",
+
+        # Build Tools & Task Runners
+        "Webpack",
+        "Vite",
+        "Parcel",
+        "Rollup",
+        "Gulp",
+        "Grunt",
+        "esbuild",
+        "Babel",
+
+        # Web Assembly & Related
+        "WebAssembly (WASM)",
+        "Blazor",
+
+        # Templating Engines
+        "Handlebars",
+        "Pug (formerly Jade)",
+        "EJS",
+        "Jinja2",
+
+        # Static Site Generators
+        "Next.js",
+        "Gatsby",
+        "Nuxt.js",
+        "Hugo",
+        "Jekyll",
+        "Eleventy (11ty)",
+        "Astro",
+
+        # Testing
+        "Jest",
+        "Mocha",
+        "Chai",
+        "Jasmine",
+        "Cypress",
+        "Playwright",
+        "Storybook",
+        "React Testing Library",
+
+        # Others
+        "HTML5",
+        "CSS3",
+        "JavaScript (ES6+)",
+        "TypeScript",
+        "D3.js",
+        "Three.js",
+        "PWA (Progressive Web Apps)",
+        "WebSockets",
+        "WebRTC"
+    ],
+    "backend": [
+        # Frameworks by Language
+        # Python
+        "Django",
+        "Flask",
+        "FastAPI",
+        "Tornado",
+        "Sanic",
+        # JavaScript/Node.js
+        "Node.js",
+        "Express.js",
+        "Koa",
+        "NestJS",
+        "Hapi",
+        # Java
+        "Spring Boot",
+        "Jakarta EE (formerly Java EE)",
+        "Play Framework",
+        "Micronaut",
+        "Quarkus",
+        # C#
+        "ASP.NET Core",
+        ".NET Framework",
+        # Ruby
+        "Ruby on Rails",
+        "Sinatra",
+        # PHP
+        "Laravel",
+        "Symfony",
+        "CodeIgniter",
+        # Go
+        "Gin",
+        "Echo",
+        # Rust
+        "Actix",
+        "Rocket",
+        # Elixir
+        "Phoenix",
+
+        # Server Technologies
+        "Nginx",
+        "Apache HTTP Server",
+        "Caddy",
+        "IIS (Internet Information Services)",
+
+        # APIs & Communication
+        "RESTful APIs",
+        "GraphQL",
+        "gRPC",
+        "WebSockets",
+        "Message Queues (RabbitMQ, Kafka, SQS)",
+        "SOAP",
+
+        # Serverless Computing
+        "AWS Lambda",
+        "Google Cloud Functions",
+        "Azure Functions",
+        "Vercel Functions",
+        "Netlify Functions",
+
+        # Authentication & Authorization
+        "OAuth 2.0",
+        "OpenID Connect (OIDC)",
+        "JWT (JSON Web Tokens)",
+        "SAML",
+
+        # Caching
+        "Redis",
+        "Memcached"
+    ],
+    "database": [
+        # Relational (SQL)
+        "PostgreSQL",
+        "MySQL",
+        "MariaDB",
+        "Microsoft SQL Server",
+        "Oracle Database",
+        "SQLite",
+        "CockroachDB",
+        "TiDB",
+
+        # NoSQL
+        # Document
+        "MongoDB",
+        "CouchDB",
+        "Firestore",
+        "DynamoDB",
+        # Key-Value
+        "Redis",
+        "Riak",
+        "etcd",
+        # Column-Family
+        "Apache Cassandra",
+        "HBase",
+        # Graph
+        "Neo4j",
+        "ArangoDB",
+        "Amazon Neptune",
+
+        # Search Engines
+        "Elasticsearch",
+        "OpenSearch",
+        "Algolia",
+        "MeiliSearch",
+        "Typesense",
+
+        # Time Series
+        "InfluxDB",
+        "TimescaleDB",
+        "Prometheus",
+
+        # NewSQL / Distributed SQL
+        "Google Spanner",
+        "YugabyteDB",
+        "VoltDB",
+
+        # Vector Databases
+        "Pinecone",
+        "Weaviate",
+        "Milvus",
+        "Chroma",
+        "Qdrant",
+
+        # Object-Relational Mapping (ORM)
+        "SQLAlchemy (Python)",
+        "Django ORM (Python)",
+        "TypeORM (TypeScript/JavaScript)",
+        "Prisma (TypeScript/JavaScript)",
+        "Hibernate (Java)",
+        "Entity Framework Core (C#)",
+        "GORM (Go)",
+        "Active Record (Ruby)"
+    ],
+    "language": [
+        # General Purpose
+        "Python",
+        "JavaScript",
+        "Java",
+        "C#",
+        "C++",
+        "C",
+        "Go",
+        "Ruby",
+        "PHP",
+        "Swift",
+        "Kotlin",
+        "Rust",
+        "TypeScript",
+        "Scala",
+        "Perl",
+        "Lua",
+        "Haskell",
+        "Elixir",
+        "Erlang",
+        "Clojure",
+        "F#",
+        "Dart",
+        "R",
+        "Julia",
+        "OCaml",
+        "Groovy",
+        "Lisp",
+        "Scheme",
+        "Objective-C",
+        "Assembly",
+        "Zig",
+
+        # Domain-Specific / Query Languages
+        "SQL",
+        "HTML",
+        "CSS",
+        "Bash",
+        "PowerShell",
+        "MATLAB",
+        "Solidity"
+    ],
+    "tools": [
+        # Version Control
+        "Git",
+        "GitHub",
+        "GitLab",
+        "Bitbucket",
+        "Subversion (SVN)",
+
+        # Containerization & Orchestration
+        "Docker",
+        "Kubernetes",
+        "Podman",
+        "Docker Swarm",
+        "OpenShift",
+        "Nomad",
+
+        # CI/CD (Continuous Integration/Continuous Deployment)
+        "Jenkins",
+        "GitHub Actions",
+        "GitLab CI/CD",
+        "CircleCI",
+        "Travis CI",
+        "TeamCity",
+        "Argo CD",
+        "Flux",
+
+        # Cloud Providers
+        "Amazon Web Services (AWS)",
+        "Google Cloud Platform (GCP)",
+        "Microsoft Azure",
+        "Heroku",
+        "DigitalOcean",
+        "Vercel",
+        "Netlify",
+
+        # Infrastructure as Code (IaC)
+        "Terraform",
+        "Ansible",
+        "Pulumi",
+        "Chef",
+        "Puppet",
+        "AWS CloudFormation",
+
+        # Monitoring & Observability
+        "Prometheus",
+        "Grafana",
+        "Datadog",
+        "New Relic",
+        "Splunk",
+        "ELK Stack (Elasticsearch, Logstash, Kibana)",
+        "Jaeger",
+        "Zipkin",
+
+        # Code Editors & IDEs
+        "Visual Studio Code (VS Code)",
+        "JetBrains IDEs (IntelliJ IDEA, PyCharm, WebStorm, etc.)",
+        "Visual Studio",
+        "Sublime Text",
+        "Atom",
+        "Vim",
+        "Neovim",
+        "Emacs",
+
+        # Project Management & Collaboration
+        "Jira",
+        "Confluence",
+        "Trello",
+        "Asana",
+        "Slack",
+        "Microsoft Teams",
+        "Notion",
+
+        # API Development & Testing
+        "Postman",
+        "Insomnia",
+        "Swagger / OpenAPI",
+
+        # Package Managers
+        "npm (Node.js)",
+        "Yarn (Node.js)",
+        "pnpm (Node.js)",
+        "pip (Python)",
+        "Maven (Java)",
+        "Gradle (Java)",
+        "NuGet (C#)",
+        "Composer (PHP)",
+        "RubyGems (Ruby)",
+        "Cargo (Rust)",
+        "Homebrew (macOS)",
+    ]
+}
